0484697dc7a851b94a2d0940fc74d322a47ba5e0
EnasAsmar <EnasAsmar@users.noreply.github.com>
2023-07-12 23:29:57 +0000

    Fix styling

--- src/database/seeds/LicenceManagerSettingsDatabaseSeeder.php
+++ src/database/seeds/LicenceManagerSettingsDatabaseSeeder.php
@@ -14,18 +14,18 @@ class LicenceManagerSettingsDatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        \DB::table('settings')->insert([
-//            [
-//                'code' => 'licence_manager_setting',
-//                'type' => 'TEXT',
-//                'category' => 'LicenceManager',
-//                'label' => 'LicenceManager setting',
-//                'value' => 'licence_manager',
-//                'editable' => 1,
-//                'hidden' => 0,
-//                'created_at' => Carbon::now(),
-//                'updated_at' => Carbon::now(),
-//            ]
-//        ]);
+        //        \DB::table('settings')->insert([
+        //            [
+        //                'code' => 'licence_manager_setting',
+        //                'type' => 'TEXT',
+        //                'category' => 'LicenceManager',
+        //                'label' => 'LicenceManager setting',
+        //                'value' => 'licence_manager',
+        //                'editable' => 1,
+        //                'hidden' => 0,
+        //                'created_at' => Carbon::now(),
+        //                'updated_at' => Carbon::now(),
+        //            ]
+        //        ]);
     }
 }
